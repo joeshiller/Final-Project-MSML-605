2b7cea7e209102a8ef68e047b7ef3851465ca228
David Medin <david@davidmedin.com>
2026-03-26 21:18:51 -0400

    add the roc curve

--- scripts/thresholds.py
+++ scripts/thresholds.py
@@ -63,6 +63,25 @@ def main():
 
     scores, labels = load_scores_csv(input_path)
 
+    # graph!
+    import sklearn.metrics
+
+    fpr, tpr, sk_threshold = sklearn.metrics.roc_curve(labels, scores)
+    roc_auc = sklearn.metrics.auc(fpr, tpr)
+    import matplotlib.pyplot as plt
+
+    plt.title("Receiver Operating Characteristic")
+    plt.plot(fpr, tpr, "b", label="AUC = %0.2f" % roc_auc)
+    plt.legend(loc="lower right")
+    plt.plot([0, 1], [0, 1], "r--")
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel("True Positive Rate")
+    plt.xlabel("False Positive Rate")
+    plt.show()
+
+    # done graphing
+
     thresholds = np.linspace(scores.min(), scores.max(), 200)
 
     best_threshold = None
